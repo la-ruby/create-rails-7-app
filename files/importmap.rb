ccbef9cc070593c0acb0076b1e86df9fb8efe41a
Sam W <sam@sam-we.com>
2021-08-30 20:01:00 -0700

    Fix CI fail: Author identity unknown

---
+++ files/importmap.rb
@@ -0,0 +1,2 @@
+  pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/esm/popper.js"
+  pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.esm.js"
